4262c53ff3f70d179c80123766d0ddfb1d42d643
David Zwart <davidzwa@gmail.com>
2022-09-30 16:40:49 +0200

    SIdebar logo adjustment and blog addition

--- content/_data/siteconfig.js
+++ content/_data/siteconfig.js
@@ -2,36 +2,36 @@ require("dotenv").config();
 
 module.exports = {
     // Website title, shown in left sidebar and in page title
-    title: "{ rené.winkelmeyer }",
+    title: "{ Venky.Prasad }",
     // Site URL to generate absolute URLs. Used across the board.
     url: process.env.URL || "http://localhost:8080",
     // Profile image for left sidebar
     image: "/assets/images/sidebar_profile.jpg",
     // Image alt text for left sidebar
-    imageAlt: "René and Einstein",
+    imageAlt: "Prof. R.R. Venkatesha Prasad",
     // Author name, shown in left sidebar, and used in JSON-LD
-    author: "René Winkelmeyer",
+    author: "David J. Zwart",
     // Site description, shown below site image (optional)
-    description: "My kitchen sink about stuff",
+    description: "Research Profile",
     // OpenGraph default image, in case you don't have an `image`
     // set in your Markdown frontmatter; relevant for social
     // sharing.
     openGraphDefaultImage: "/assets/images/opengraph.jpg",
     // GitHub ID (optional, remove it not needed), used for link in the left sidebar
-    socialGitHub: "muenzpraeger",
+    socialGitHub: "davidzwa",
     // LinkedIn ID  (optional, remove it not needed), used for link in the left sidebar
-    socialLinkedIn: "muenzpraeger",
+    socialLinkedIn: "davidzwa",
     // Twitter ID  (optional, remove it not needed), used for link in the left sidebar, and for OpenGraph sharing information
-    socialTwitter: "muenzpraeger",
+    socialTwitter: "davidzwa",
     // YouTube ID/Channel  (optional, remove it not needed), used for link in the left sidebar
-    socialYouTube: "UCH60RRaY2GI9m62z1loLjcA",
+    // socialYouTube: "UCH60RRaY2GI9m62z1loLjcA",
     // Google Analytics ID  (optional, remove it not needed), used for... well, Google Analytics
-    googleAnalytics: "YOUR_GA-ID",
+    // googleAnalytics: "YOUR_GA-ID",
     // Algolia-powered search  (optional, remove it not needed),
     // See: https://github.com/algolia/algoliasearch-netlify
     algoliaSearch: {
         // When enabled shows the search bar in the UI
-        enabled: true,
+        enabled: false,
         // You'll have to set this manually in your build settings.
         // The value comes from Algolia, and is either visible in the
         // UI for the Crawler Plugin or the Algolia Dashboard.
---
+++ content/posts/2022/vp-launches-website.md
@@ -0,0 +1,9 @@
+---
+title: VP has launched his new rocket!
+date: 2022-09-30
+image: /assets/images/Ranga-Rao-Venkatesha-Prasad.jpg
+---
+
+Together with SpaZED, VP has launched his new batteryless rocket!
+
+Go check it out here: Yoo Toob link
